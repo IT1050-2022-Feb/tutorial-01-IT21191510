94b33258b745e00731934385d435d40917b8a2c8
IT21191510 <99599176+IT21191510@users.noreply.github.com>
2022-02-19 14:59:51 +0000

    completed tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1,mark2;
+  float avg;
+
+  printf("Enter the mark of 1st subject : ");
+  scanf("%d", &mark1);
+
+  printf("Enter the mark of 2st subject : ");
+  scanf("%d", &mark2);
+
+  avg = (mark1 + mark2)/2;
+
+  printf("The Average of the two marks is : %.2f ",avg);
   
   return 0;
 }
